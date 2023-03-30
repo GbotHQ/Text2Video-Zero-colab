1b31f2708fbcc856ac70da118512b006751b9ba7
GbotHQ <tombrum9@gmail.com>
2023-03-30 16:35:51 +0200

    Move demo code to app.py from notebook

--- Text2Video-Zero/app.py
+++ Text2Video-Zero/app.py
@@ -1,18 +1,14 @@
 import gradio as gr
 import torch
 
-from model import Model, ModelType
+from model import Model
 
 from app_canny import create_demo as create_demo_canny
-from app_pose import create_demo as create_demo_pose
-from app_text_to_video import create_demo as create_demo_text_to_video
-from app_pix2pix_video import create_demo as create_demo_pix2pix_video
-from app_canny_db import create_demo as create_demo_canny_db
 
 
-model = Model(device='cuda', dtype=torch.float16)
+model = Model(device="cuda", dtype=torch.float16)
 
-with gr.Blocks(css='style.css') as demo:
+with gr.Blocks(css="style.css") as demo:
     gr.HTML(
         """
         <div style="text-align: center; max-width: 1200px; margin: 20px auto;">
@@ -36,26 +32,11 @@ with gr.Blocks(css='style.css') as demo:
         We hope our Text2Video-Zero will further democratize AI and empower the creativity of everyone by unleashing the zero-shot video generation and editing capacity of the amazing text-to-image models and encourage future research!
         </h2>
         </div>
-        """)
+        """
+    )
 
-    
-    gr.HTML("""
-    <p>For faster inference without waiting in queue, you may duplicate the space and upgrade to GPU in settings.
-    <br/>
-    <a href="https://huggingface.co/spaces/PAIR/Text2Video-Zero?duplicate=true">
-    <img style="margin-top: 0em; margin-bottom: 0em" src="https://bit.ly/3gLdBN6" alt="Duplicate Space"></a>
-    </p>""")
-
-    with gr.Tab('Zero-Shot Text2Video'):
-        create_demo_text_to_video(model)
-    with gr.Tab('Video Instruct Pix2Pix'):
-        create_demo_pix2pix_video(model)
-    with gr.Tab('Pose Conditional'):
-        create_demo_pose(model)
-    with gr.Tab('Edge Conditional'):
+    with gr.Tab("Edge Conditional"):
         create_demo_canny(model)
-    with gr.Tab('Edge Conditional and Dreambooth Specialized'):
-        create_demo_canny_db(model)
 
     gr.HTML(
         """
@@ -76,11 +57,8 @@ with gr.Blocks(css='style.css') as demo:
         Text2Video-Zero in this demo is meant only for research purposes.
         </h3>
         </div>
-        """)
+        """
+    )
 
-# demo.launch(share=True)
-# demo.launch(debug=True)
-
-_, _, link = demo.queue(api_open=False).launch(
-    file_directories=['temporal'], share=args.public_access)
+_, _, link = demo.launch(file_directories=["temporal"], share=True)
 print(link)
