e053b9d4996fc4b54f7837e3f85f79da778c37e7
GbotHQ <tombrum9@gmail.com>
2023-03-30 17:37:35 +0200

    Remove unused code

--- Text2Video-Zero/app.py
+++ Text2Video-Zero/app.py
@@ -11,5 +11,5 @@ model = Model(device="cuda", dtype=torch.float16)
 with gr.Blocks(css="style.css") as demo:
     create_demo_canny(model)
 
-_, _, link = demo.launch(file_directories=["temporal"], share=True)
+_, _, link = demo.launch(file_directories=["temporal"], debug=True, share=True)
 print(link)
